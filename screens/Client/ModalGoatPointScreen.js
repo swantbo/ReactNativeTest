940b77247748e0ade26f5b3ed4ef4cbaacf6c45a
andrewchester1 <49591047+andrewchester1@users.noreply.github.com>
2021-11-19 14:53:36 -0600

    text for goat points

--- screens/Client/ModalGoatPointScreen.js
+++ screens/Client/ModalGoatPointScreen.js
@@ -8,10 +8,14 @@ const ModalGoatPointScreen = () => {
     return (
         <View style={styles.container}>
             <Card containerStyle={{ flex: 1, backgroundColor: '#121212', borderColor: '#121212'}}>
-                <Card.Title style={styles.text}>Image Title</Card.Title>
+                <Card.Title style={styles.text}>Goat Points</Card.Title>
                 <Card.Divider/>
                 <View>
-                    <Text>Test</Text>
+                    <Text>Goat Points are a currency that can be used for Haircuts</Text>
+                    <Text>1 Goat Point is equal to 1 cent</Text>
+                    <Text>So 500 Goat Points is $5.00</Text>
+                    <Text>When setting up an appointment you can use your Goat Points to get money off your haircut</Text>
+                    <Text>To earn Goat Points talk to your Barber!</Text>
                 </View>
             </Card>
         </View>

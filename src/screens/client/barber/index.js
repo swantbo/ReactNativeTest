401b83c8a6bfd69ded7e5f5a08c9138bfdb43fc3
andrewchester1 <49591047+andrewchester1@users.noreply.github.com>
2021-12-16 14:34:12 -0600

    test

--- src/screens/client/barber/index.js
+++ src/screens/client/barber/index.js
@@ -34,7 +34,7 @@ function BarberScreen(props) {
 			// const urls = await Promise.all(
 			//     imageRefs.items.map((ref) => ref.getDownloadURL())
 			// )
-			const urls = []
+			const urls = ['https://picsum.photos/200/300?random=1', 'https://picsum.photos/200/300?random=2', 'https://picsum.photos/200/300?random=3', 'https://picsum.photos/200/300?random=4']
 			setHaircutImages(urls)
 		}
 
@@ -52,161 +52,162 @@ function BarberScreen(props) {
 			</Card>
 
 			<View style={styles.container}>
-				<ScrollView style={styles.scrollView}>
-					<Card containerStyle={styles.cardBio}>
-						<Card.Title style={styles.cardTitle}>{barber.name}</Card.Title>
-						<Card.Title style={styles.listItemSubTitle}>{barber.bio}</Card.Title>
-					</Card>
-					<View style={styles.row}>
-						<View style={{flex: 1}}>
-							<PricingCard
-								containerStyle={styles.pricingCard}
-								pricingStyle={styles.listItemSubTitle}
-								color='#E8BD70'
-								title={<Card.Title style={styles.cardTitle}>Men's Haircut</Card.Title>}
-								price={<Card.Title style={styles.barberPricing}>{barber.price}</Card.Title>}
-								info={['Includes Haircut, Eyebrows and Beard trim']}
-								button={{
-									title: 'Schedule Now'
-								}}
-								onButtonPress={() => {
-									props.navigation.navigate('Appointment')
-								}}
-							/>
-						</View>
-						<View style={{flex: 1}}>
-							<PricingCard
-								containerStyle={styles.pricingCard}
-								pricingStyle={styles.listItemSubTitle}
-								color='#E8BD70'
-								title={<Card.Title style={styles.cardTitle}>Kid's Haircut</Card.Title>}
-								price={<Card.Title style={styles.barberPricing}>{barber.kidsHaircut}</Card.Title>}
-								info={["Includes Full Haircut, for Kid's"]}
-								button={{
-									title: 'Schedule Now'
-								}}
-								onButtonPress={() => {
-									props.navigation.navigate('Appointment')
-								}}
-							/>
-						</View>
-					</View>
-					<View style={styles.barberSocialIcons}>
-						<SocialIcon
-							onPress={() => {
-								props.navigation.navigate('Appointment')
-							}}
-							style={styles.socialIcons}
-							iconType='MaterialCommunityIcons'
-							type='calendar-today'
-						/>
-						<SocialIcon
-							onPress={() =>
-								Linking.openURL(`sms:${barber?.phone}`).catch(() => {
-									Linking.openURL(`sms:${barber?.phone}`)
-								})
-							}
-							style={styles.socialIcons}
-							iconType='MaterialCommunityIcons'
-							type='sms'
-						/>
-						<SocialIcon
-							onPress={() =>
-								Linking.openURL(`instagram://user?username=${barber.instagram}`).catch(() => {
-									Linking.openURL(`https://www.instagram.com/${barber.instagram}`)
-								})
-							}
-							style={styles.socialIcons}
-							type='instagram'
-						/>
-						<SocialIcon
-							onPress={() =>
-								Linking.openURL(`${barber.website}`).catch(() => {
-									Linking.openURL(`https://${barber.website}`)
-								})
-							}
-							style={styles.socialIcons}
-							type='google'
-							title='test'
-						/>
-					</View>
-
-					<Card containerStyle={styles.barberAddress}>
-						<Card.Title style={{alignSelf: 'flex-start', color: '#E8BD70'}}>ADDRESS & HOURS</Card.Title>
-						<View style={styles.row}>
-							<View style={styles.rowStart}>
-								<Text style={styles.addressText}>{barber.location}</Text>
-								<TouchableOpacity
+				<FlatList
+					style={styles.scrollView}
+					ListHeaderComponent={
+						<>
+							<Card containerStyle={styles.cardBio}>
+								<Card.Title style={styles.cardTitle}>{barber.name}</Card.Title>
+								<Card.Title style={styles.listItemSubTitle}>{barber.bio}</Card.Title>
+							</Card>
+							<View style={styles.row}>
+								<View style={{flex: 1}}>
+									<PricingCard
+										containerStyle={styles.pricingCard}
+										pricingStyle={styles.listItemSubTitle}
+										color='#E8BD70'
+										title={<Card.Title style={styles.cardTitle}>Men's Haircut</Card.Title>}
+										price={<Card.Title style={styles.barberPricing}>{barber.price}</Card.Title>}
+										info={['Includes Haircut, Eyebrows and Beard trim']}
+										button={{
+											title: 'Schedule Now'
+										}}
+										onButtonPress={() => {
+											props.navigation.navigate('Appointment')
+										}}
+									/>
+								</View>
+								<View style={{flex: 1}}>
+									<PricingCard
+										containerStyle={styles.pricingCard}
+										pricingStyle={styles.listItemSubTitle}
+										color='#E8BD70'
+										title={<Card.Title style={styles.cardTitle}>Kid's Haircut</Card.Title>}
+										price={<Card.Title style={styles.barberPricing}>{barber.kidsHaircut}</Card.Title>}
+										info={["Includes Full Haircut, for Kid's"]}
+										button={{
+											title: 'Schedule Now'
+										}}
+										onButtonPress={() => {
+											props.navigation.navigate('Appointment')
+										}}
+									/>
+								</View>
+							</View>
+							<View style={styles.barberSocialIcons}>
+								<SocialIcon
+									onPress={() => {
+										props.navigation.navigate('Appointment')
+									}}
+									style={styles.socialIcons}
+									iconType='MaterialCommunityIcons'
+									type='calendar-today'
+								/>
+								<SocialIcon
 									onPress={() =>
 										Linking.openURL(`sms:${barber?.phone}`).catch(() => {
 											Linking.openURL(`sms:${barber?.phone}`)
 										})
-									}>
-									<Text style={styles.addressText}>{barber.phone != '' ? formatPhoneNumber(barber.phone) : ''}</Text>
-								</TouchableOpacity>
-								<Text></Text>
-								<View style={styles.row}>
-									<Text style={styles.addressTitle}>Tuesday</Text>
-									<Text style={styles.addressText}>{' ' + barber.Tuesday}</Text>
-								</View>
-								<View style={styles.row}>
-									<Text style={styles.addressTitle}>Wednesday</Text>
-									<Text style={styles.addressText}>{' ' + barber.Wednesday}</Text>
-								</View>
-								<View style={styles.row}>
-									<Text style={styles.addressTitle}>Thursday</Text>
-									<Text style={styles.addressText}>{' ' + barber.Thursday}</Text>
-								</View>
-								<View style={styles.row}>
-									<Text style={styles.addressTitle}>Friday</Text>
-									<Text style={styles.addressText}>{' ' + barber.Friday}</Text>
-								</View>
-								<View style={styles.row}>
-									<Text style={styles.addressTitle}>Saturday</Text>
-									<Text style={styles.addressText}>{' ' + barber.Saturday}</Text>
-								</View>
-							</View>
-							<View style={styles.rowEnd}>
-								<MapView
-									style={{width: '100%', height: '100%'}}
-									region={{
-										latitude: 43.0218740049977,
-										longitude: -87.9119389619647,
-										latitudeDelta: 0.005,
-										longitudeDelta: 0.005
-									}}
-									pitchEnabled={false}
-									rotateEnabled={false}
-									scrollEnabled={false}
-									zoomEnabled={false}
+									}
+									style={styles.socialIcons}
+									iconType='MaterialCommunityIcons'
+									type='sms'
+								/>
+								<SocialIcon
 									onPress={() =>
-										Linking.openURL('maps://app?saddr=&daddr=43.0218740049977+-87.9119389619647').catch(() => {
-											Linking.openURL('google.navigation:q=43.0218740049977+-87.9119389619647')
+										Linking.openURL(`instagram://user?username=${barber.instagram}`).catch(() => {
+											Linking.openURL(`https://www.instagram.com/${barber.instagram}`)
 										})
-									}>
-									<MapView.Marker coordinate={{latitude: 43.0218740049977, longitude: -87.9119389619647}} />
-								</MapView>
+									}
+									style={styles.socialIcons}
+									type='instagram'
+								/>
+								<SocialIcon
+									onPress={() =>
+										Linking.openURL(`${barber.website}`).catch(() => {
+											Linking.openURL(`https://${barber.website}`)
+										})
+									}
+									style={styles.socialIcons}
+									type='google'
+									title='test'
+								/>
 							</View>
-						</View>
-					</Card>
 
-					<Card containerStyle={styles.barberAddress}>
-						<Card.Title style={{alignSelf: 'flex-start', color: '#E8BD70'}}>Photos</Card.Title>
-						<View style={styles.containerGallery}>
-							<FlatList
-								nestedScrollEnabled
-								numColumns={2}
-								horizontal={false}
-								data={haircutImages}
-								renderItem={({item}) => (
-									<View style={styles.containerImage}>
-										<Image style={styles.image} source={{uri: item}} />
+							<Card containerStyle={styles.barberAddress}>
+								<Card.Title style={{alignSelf: 'flex-start', color: '#E8BD70'}}>ADDRESS & HOURS</Card.Title>
+								<View style={styles.row}>
+									<View style={styles.rowStart}>
+										<Text style={styles.addressText}>{barber.location}</Text>
+										<TouchableOpacity
+											onPress={() =>
+												Linking.openURL(`sms:${barber?.phone}`).catch(() => {
+													Linking.openURL(`sms:${barber?.phone}`)
+												})
+											}>
+											<Text style={styles.addressText}>{barber.phone != '' ? formatPhoneNumber(barber.phone) : ''}</Text>
+										</TouchableOpacity>
+										<Text></Text>
+										<View style={styles.row}>
+											<Text style={styles.addressTitle}>Tuesday</Text>
+											<Text style={styles.addressText}>{' ' + barber.Tuesday}</Text>
+										</View>
+										<View style={styles.row}>
+											<Text style={styles.addressTitle}>Wednesday</Text>
+											<Text style={styles.addressText}>{' ' + barber.Wednesday}</Text>
+										</View>
+										<View style={styles.row}>
+											<Text style={styles.addressTitle}>Thursday</Text>
+											<Text style={styles.addressText}>{' ' + barber.Thursday}</Text>
+										</View>
+										<View style={styles.row}>
+											<Text style={styles.addressTitle}>Friday</Text>
+											<Text style={styles.addressText}>{' ' + barber.Friday}</Text>
+										</View>
+										<View style={styles.row}>
+											<Text style={styles.addressTitle}>Saturday</Text>
+											<Text style={styles.addressText}>{' ' + barber.Saturday}</Text>
+										</View>
 									</View>
-								)}
-							/>
-						</View>
-					</Card>
-				</ScrollView>
+									<View style={styles.rowEnd}>
+										<MapView
+											style={{width: '100%', height: '100%'}}
+											region={{
+												latitude: 43.0218740049977,
+												longitude: -87.9119389619647,
+												latitudeDelta: 0.005,
+												longitudeDelta: 0.005
+											}}
+											pitchEnabled={false}
+											rotateEnabled={false}
+											scrollEnabled={false}
+											zoomEnabled={false}
+											onPress={() =>
+												Linking.openURL('maps://app?saddr=&daddr=43.0218740049977+-87.9119389619647').catch(() => {
+													Linking.openURL('google.navigation:q=43.0218740049977+-87.9119389619647')
+												})
+											}>
+											<MapView.Marker coordinate={{latitude: 43.0218740049977, longitude: -87.9119389619647}} />
+										</MapView>
+									</View>
+								</View>
+							</Card>
+						</>
+					}
+					numColumns={2}
+					horizontal={false}
+					data={haircutImages}
+					renderItem={({item}) => (
+						<Card containerStyle={styles.barberAddress}>
+							<Card.Title style={{alignSelf: 'flex-start', color: '#E8BD70'}}>Photos</Card.Title>
+							<View style={styles.containerGallery}>
+								<View style={styles.containerImage}>
+									<Image style={styles.image} source={{uri: item}} />
+								</View>
+							</View>
+						</Card>
+					)}
+				/>
 			</View>
 		</>
 	)
